74dda883d8e522dfa128c3c8186de947f2e5cace
Radhe Raman Tiwari <radheramantiwari87@gmail.com>
2019-06-30 02:33:26 +0530

    initial Commit

---
+++ models/research/object_detection/transfer_image_resolution.py
@@ -0,0 +1,19 @@
+from PIL import Image
+import os
+import argparse
+
+def rescale_images(directory, size):
+	for img in os.listdir(directory):
+		try :
+        		im = Image.open(directory+img)
+        		im_resized = im.resize(size, Image.ANTIALIAS)
+        		im_resized.save(directory+img)
+		except Exception as e :
+			pass
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Rescale images")
+    parser.add_argument('-d', '--directory', type=str, required=True, help='Directory containing the images')
+    parser.add_argument('-s', '--size', type=int, nargs=2, required=True, metavar=('width', 'height'), help='Image size')
+    args = parser.parse_args()
+    rescale_images(args.directory, args.size)
